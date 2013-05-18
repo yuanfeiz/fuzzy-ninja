7960364a4bc21abf24187a5a3e9acade57202761
stranbird <meteey@gmail.com>
2013-05-18 15:44:19 +0800

    init ios project

---
+++ FuzzyNinja/FuzzyNinja/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FuzzyNinja
+//
+//  Created by stranbird on 13-5-18.
+//  Copyright (c) 2013年 stranbird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FuzzyNinja/FuzzyNinja/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FuzzyNinja
+//
+//  Created by stranbird on 13-5-18.
+//  Copyright (c) 2013年 stranbird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
